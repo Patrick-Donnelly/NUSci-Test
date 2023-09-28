431a024d09fc2c1b580d4154f429b4001e306a11
Patrick Donnelly <PatrickDonnelly32103@gmail.com>
2023-09-27 21:20:57 -0400

    Decentralized app into app.js

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
-import express from 'express';
-import cors from 'cors';
-import helmet from 'helmet';
-import route from './pages.route.js';
-
-const app = express();
-
-app.use(cors());
-app.use(helmet());
-app.use(express.json());
-
-app.use('/', route);
-app.use('/page2', route);
+import app from './app.js';
 
 const HOSTNAME = '0.0.0.0';
 const PORT = 3000;
