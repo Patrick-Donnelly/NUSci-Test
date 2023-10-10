ad6318e00bf3c53f235830808f9ccebcdcca3b0d
Patrick Donnelly <PatrickDonnelly32103@gmail.com>
2023-10-09 21:07:24 -0400

    Phase 4: EJS

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -4,6 +4,6 @@ import UserAccessor from '../db_accessor/user.accessor.js';
 export default class UserController {
     static async getAllUsers(req, res) {
         const users = await UserAccessor.getAllUsers();
-        res.json(users);
+        res.render('index', {users: users});
     }
 }
