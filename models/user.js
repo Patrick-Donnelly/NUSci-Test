fa1a46e914c71ccd20fc3b2806bf47394f218009
Patrick Donnelly <PatrickDonnelly32103@gmail.com>
2023-10-09 20:17:56 -0400

    User info fix

--- models/user.js
+++ models/user.js
@@ -4,9 +4,11 @@ const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
     username: {type: String, unique: true, required: true},
-    password: {type: String, unique: true},
+    password: {type: String, unique: false, required: true},
     email: {type: String, unique: true, required: true},
-    bio: {type: String, unique: true}
+    bio: {type: String, unique: true, required: false},
+    followers: [{type: String}],
+    following: [{type: String}]
 }, {
     collection: 'users'
 });
